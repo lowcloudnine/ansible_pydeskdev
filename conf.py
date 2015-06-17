38f0e8b438da21186a6c6b1d6f01dee466b80a0d
Jon Schiefelbein <lowcloudnine@hotmail.com>
2015-06-17 15:30:19 -0400

    reverted conf.py
    Reverted conf.py back to using index.rst

--- conf.py
+++ conf.py
@@ -47,7 +47,7 @@ source_suffix = ['.rst', '.md']
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'README.md'
+master_doc = 'index'
 
 # General information about the project.
 project = u'Python Desktop Development Environment Setup'
